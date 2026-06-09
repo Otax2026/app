b97f80ff39c9b76db445fcaebea40b2351acb510
Otax2026 <info@offertax.com>
2026-06-08 18:55:50 -0700

    Update mappings.js

--- mappings.js
+++ mappings.js
@@ -1,43 +1,57 @@
-// mappings.js — hosted on GitHub
-// This file ONLY contains form field selectors.
-// It is completely useless without the API key stored locally in the extension.
-// Safe to share publicly — contains no credentials, no client data.
-
-var MAPPINGS = {
-  monterey: {
-    name: "Monterey Financial",
-    hostMatch: "montereyfinancial.com",
-    fields: [
-      { from: "FirstName", to: "[name='applicant_firstname']" },
-      { from: "LastName",  to: "[name='applicant_lastname']" },
-      { from: "CellPhone", to: "[name='applicant_cellphone']" },
-      { from: "HomePhone", to: "[name='applicant_msgphone']" },
-      { from: "Email",     to: "[name='applicant_email']" },
-      { from: "Address",   to: "[name='applicant_streetaddress']" },
-      { from: "City",      to: "[name='applicant_city']" },
-      { from: "State",     to: "[name='applicant_state']" },
-      { from: "Zip",       to: "[name='applicant_zip']" },
-      { from: "SSN",       to: "[name='applicant_socialsecurity']" },
-      { from: "DOB",       to: "[name='applicant_dob']", type: "date" }
+{
+  "monterey": {
+    "name": "Monterey Financial",
+    "hostMatch": "montereyfinancial.com",
+    "fields": [
+      { "from": "FirstName", "to": "[name='applicant_firstname']" },
+      { "from": "LastName",  "to": "[name='applicant_lastname']" },
+      { "from": "CellPhone", "to": "[name='applicant_cellphone']" },
+      { "from": "HomePhone", "to": "[name='applicant_msgphone']" },
+      { "from": "Email",     "to": "[name='applicant_email']" },
+      { "from": "Address",   "to": "[name='applicant_streetaddress']" },
+      { "from": "City",      "to": "[name='applicant_city']" },
+      { "from": "State",     "to": "[name='applicant_state']" },
+      { "from": "Zip",       "to": "[name='applicant_zip']" },
+      { "from": "SSN",       "to": "[name='applicant_socialsecurity']" },
+      { "from": "DOB",       "to": "[name='applicant_dob']", "type": "date" }
     ]
   },
-  ucfs: {
-    name: "UCFS",
-    hostMatch: "ucfs.net",
-    fields: [
-      { from: "FirstName", to: "[name='CustomerInformation.FirstName']" },
-      { from: "LastName",  to: "[name='CustomerInformation.LastName']" },
-      { from: "CellPhone", to: "#CustomerInformation_MobilePhoneNumber" },
-      { from: "HomePhone", to: "#CustomerInformation_HomePhoneNumber" },
-      { from: "Email",     to: "[name='CustomerInformation.EmailAddress']" },
-      { from: "Email",     to: "[name='CustomerInformation.ConfirmEmailAddress']" },
-      { from: "Address",   to: "[name='CustomerInformation.Address.Address1']" },
-      { from: "City",      to: "[name='CustomerInformation.Address.City']" },
-      { from: "State",     to: "[name='CustomerInformation.Address.StateOrProvince']" },
-      { from: "Zip",       to: "[name='CustomerInformation.Address.ZipCode']" },
-      { from: "SSN",       to: "[name='CustomerInformation.SocialSecurityNumber']" },
-      { from: "SSN",       to: "[name='CustomerInformation.ConfirmSocialSecurityNumber']" },
-      { from: "DOB",       to: "[name='CustomerInformation.BirthDate']", type: "date" }
+  "ucfs": {
+    "name": "UCFS",
+    "hostMatch": "ucfs.net",
+    "fields": [
+      { "from": "FirstName", "to": "[name='CustomerInformation.FirstName']" },
+      { "from": "LastName",  "to": "[name='CustomerInformation.LastName']" },
+      { "from": "CellPhone", "to": "#CustomerInformation_MobilePhoneNumber" },
+      { "from": "HomePhone", "to": "#CustomerInformation_HomePhoneNumber" },
+      { "from": "Email",     "to": "[name='CustomerInformation.EmailAddress']" },
+      { "from": "Email",     "to": "[name='CustomerInformation.ConfirmEmailAddress']" },
+      { "from": "Address",   "to": "[name='CustomerInformation.Address.Address1']" },
+      { "from": "City",      "to": "[name='CustomerInformation.Address.City']" },
+      { "from": "State",     "to": "[name='CustomerInformation.Address.StateOrProvince']" },
+      { "from": "Zip",       "to": "[name='CustomerInformation.Address.ZipCode']" },
+      { "from": "SSN",       "to": "[name='CustomerInformation.SocialSecurityNumber']" },
+      { "from": "SSN",       "to": "[name='CustomerInformation.ConfirmSocialSecurityNumber']" },
+      { "from": "DOB",       "to": "[name='CustomerInformation.BirthDate']", "type": "date" }
+    ]
+  },
+  "sfc": {
+    "name": "SFC Portal",
+    "hostMatch": "portal.sfc.us",
+    "labelBased": true,
+    "fields": [
+      { "from": "FirstName", "label": "First Name" },
+      { "from": "LastName",  "label": "Last Name" },
+      { "from": "MiddleName","label": "Middle Name" },
+      { "from": "SSN",       "label": "SSN", "dataMask": "000-00-0000" },
+      { "from": "DOB",       "label": "Birth Date", "type": "date" },
+      { "from": "Address",   "label": "Address 1" },
+      { "from": "AptNo",     "label": "Address 2" },
+      { "from": "Zip",       "label": "Zip Code" },
+      { "from": "City",      "label": "City" },
+      { "from": "Email",     "label": "Email Address" },
+      { "from": "Email",     "label": "Verify Email Address" },
+      { "from": "CellPhone", "label": "Phone Number", "phoneType": "Mobile" }
     ]
   }
-};
+}
